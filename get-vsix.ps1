3a4bd3d466cf9f47d417232e33a17bda42a617e8
Kevin Kvissberg <55594690+KevinKvissberg@users.noreply.github.com>
2025-02-10 15:44:27 +0100

    Update get-vsix.ps1

--- get-vsix.ps1
+++ get-vsix.ps1
@@ -9,7 +9,7 @@ $publisher = [Regex]::Match($url, "itemName=(.*)\.").Groups[1].value
 $extensionName = [Regex]::Match($url, "\.([\S][^.]*)$").Groups[1].value
 
 if ($null -eq $version) {
-    $version = [regex]::match(((Invoke-WebRequest -uri $url -UseBasicParsing).rawcontent),'"version":"(7.4.2)"').Groups[1].Value
+    $version = [regex]::match(((Invoke-WebRequest -uri $url -UseBasicParsing).rawcontent),'"version":"([\d.]*)"').Groups[1].Value
 }
 
 $vsixUrl = "https://$publisher.gallery.vsassets.io/_apis/public/gallery/publisher/$publisher/extension/$extensionName/$version/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
